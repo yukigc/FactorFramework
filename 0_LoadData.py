55562a57f62d83a8c92eff59ae2be5fd616ae1cd
yukigc <gongchu0825@gmail.com>
2019-11-08 15:19:39 +0800

    load data v0

--- 0_LoadData.py
+++ 0_LoadData.py
@@ -8,20 +8,43 @@ import copy
 
 
 # %%
-def get_df(variable, tname ,Daily_return_with_cap, timeindex_day, stock_list, stocks_locate_cum):
+def get_df(variable, tname, initial_data, timeindex_day, stock_list, stocks_locate_cum, start_day, end_day):
+    '''
+    tansform series to dataframe
+    variable: get a specific variable, like close, totalshare...
+    tname: column name of date in initial data, 'TRADE_DT'
+    timeindex_day, stock_list: index & columns
+    stocks_locate_cum: locate by sorted order
+    '''
     df = pd.DataFrame(index = timeindex_day, columns = stock_list)
-    df = df.loc['1999-01-01':'2016-12-31', :]
-
-    def stock_match(variable,tname, Daily_return_with_cap, s, stocks_locate_cum, timeindex):
-        temp = Daily_return_with_cap.loc[stocks_locate_cum.loc[s.name,'start']:stocks_locate_cum.loc[s.name,'end'], [tname,variable]]
+    df = df.loc[start_day:end_day, :]
+    def stock_match(variable,tname, initial_data, s, stocks_locate_cum, timeindex):
+        temp = initial_data.loc[stocks_locate_cum.loc[s.name,'start']:stocks_locate_cum.loc[s.name,'end'], [tname,variable]]
         temp = temp.drop_duplicates(tname)
         temp_df = pd.DataFrame(data = temp[variable].values, index = temp[tname], columns = [variable])
         result = pd.DataFrame(index = timeindex, columns = [variable])
         result[variable] = temp_df[variable] 
         return result[variable].values
 
-    df = df.apply(lambda s: stock_match(variable,tname,Daily_return_with_cap, s, stocks_locate_cum, df.index))
+    df = df.apply(lambda s: stock_match(variable,tname, initial_data, s, stocks_locate_cum, df.index))
     return df
     
+
+
+def get_locate_cum(initial_data):
+    '''
+    locate by sorted order
+    '''
+    stocks_locate = initial_data[['TRADE_DT','S_INFO_WINDCODE']].groupby(by=['S_INFO_WINDCODE']).count()
+    stocks_locate_cum = stocks_locate.cumsum()
+
+    stocks_locate_cum['start'] = np.nan
+    stocks_locate_cum['end'] = np.nan
+    stocks_locate_cum['start'] = stocks_locate_cum['TRADE_DT'].shift(1)
+    stocks_locate_cum.loc['000001.SZ', 'start'] = 0
+    stocks_locate_cum['start'] = stocks_locate_cum['start'].astype(int)
+    stocks_locate_cum['end'] = stocks_locate_cum['TRADE_DT']-1
+    return stocks_locate_cum
+
 
 # %%
